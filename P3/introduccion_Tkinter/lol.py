f4bd5131945a20da9fd3dfd195c99363dfa246e9
AntooGuevara <jose_3141240215@utd.edu.mx>
2025-11-30 19:00:21 -0600

    Delete P3 directory

--- P3/introduccion_Tkinter/lol.py
+++
@@ -1,8 +0,0 @@
-class Personas:
-    def __init__(self, nombre, edad, tel):
-        self.nombre = nombre
-        self.edad = edad
-        self.tel = tel
-    
-    def info_persona(self):
-        return f"Nombre: {self.nombre}, Edad: {self.edad}, Tel: {self.tel}"
