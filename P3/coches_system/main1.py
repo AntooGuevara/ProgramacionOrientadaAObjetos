a9da65e3060c36edc9c0465bdf8ebae4486dd32f
AntooGuevara <jose_3141240215@utd.edu.mx>
2025-12-03 21:42:49 -0600

    Delete P3/coches_system directory

--- P3/coches_system/main1.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-from view.view_1 import View  # importar la clase, no el módulo
-
-if __name__ == "__main__":
-    ventana = Tk()
-    View(ventana)   # ✔️ ahora sí
-    ventana.mainloop()
